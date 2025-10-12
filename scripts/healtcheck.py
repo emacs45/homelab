1f4b9daa1a8a5e85fdae274b2e594f130a295c26
emacs45 <emec.acmi@gmail.com>
2025-10-12 19:01:42 +0200

    feat: add healthcheck

--- scripts/healtcheck.py
+++ scripts/healtcheck.py
@@ -1,13 +1,15 @@
-errors = []
+import socket 
 
-with open('access.log', 'r') as infile, open('errors.log', 'w') as outfile:
-    for line in infile:
-        line = line.strip()
-        if '500' in line:
-            errors.append(line)
-    outfile.write("Gefundene 500-Fehler:\n")
-    
-    for e in errors:
-        outfile.write(e)
-        outfile.write('\n')
-    outfile.write(f'\n\nAnzahl: {len(errors)}')
+with open('targets.txt', 'r') as hosts: 
+    for host in hosts: 
+        ip, port = host.strip().split(':') 
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+        print (f'Attempting to connect to {ip} on port {port}...') 
+        
+        try: 
+            s.connect((ip, int(port))) 
+            print(f'{ip} on port {port} is available ✅') 
+        except socket.error as error: 
+            print(f'{ip} on port {port} failed: {error} ❌') 
+        
+        finally: s.close()
